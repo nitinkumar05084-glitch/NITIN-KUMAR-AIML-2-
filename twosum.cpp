0a3811626ab13361f2a6992040e3a8e5e6af3a9d
nitinkumar05084-glitch <nitinkumar05084@gmail.com>
2026-01-13 20:28:20 +0530

    Add files via upload

---
+++ twosum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={2,7,11,15};
+    int n = 4;
+    int target = 9;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] + arr[j] == target){
+                cout<<"index of i:"<<i<<endl<<"index of j:"<<j;
+            }
+        }
+    }
+    return 0;
+}
